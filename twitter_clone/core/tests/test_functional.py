6f2d72c633e269cc8c296f214ccf47c19975a849
tmcothran <tmcothran@hotmail.com>
2016-09-27 11:28:16 -0400

    Adds initial tests, wires up homepage (but without a template).

--- twitter_clone/core/tests/test_functional.py
+++ twitter_clone/core/tests/test_functional.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.test import LiveServerTestCase
+
+from selenium import webdriver
 
 # Create your tests here.
+
+class NewVisitorTest(LiveServerTestCase):
+    """Tests the experience of a prospective new user, Jerry."""
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    # Jerry, a new user, navigates to the home page
+    def test_whether_new_user_sees_welcome_page(self):
+        self.browser.get(self.live_server_url)
+        self.assertIn("Twitter", self.browser.title)
