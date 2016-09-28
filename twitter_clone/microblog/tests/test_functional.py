62cd0bfbdffb4f39c3917b10715ed7d17679390a
tmcothran <tmcothran@hotmail.com>
2016-09-28 11:13:07 -0400

    Adds rudimentary detail page for blog entry.

--- twitter_clone/microblog/tests/test_functional.py
+++ twitter_clone/microblog/tests/test_functional.py
@@ -12,6 +12,11 @@ TEST_USER = {
     'email': 'asdklfjwe@gmail.com',
 }
 
+TEST_USER2 = {
+    'username': 'asdfl12io',
+    'password': 'sarjla3*fk(#J',
+    'email': 'klilhsfls@gmail.com',
+}
 
 class EntryCreationTest(LiveServerTestCase):
     """Tests the creation of entries in a user's microblog."""
@@ -22,12 +27,20 @@ class EntryCreationTest(LiveServerTestCase):
         self.test_user = User.objects.create_user(
             TEST_USER['username'], TEST_USER['email'], TEST_USER['password']
         )
+        self.test_user2 = User.objects.create_user(
+            TEST_USER2['username'], TEST_USER2['email'], TEST_USER2['password']
+        )
+        self.test_tweet = models.Entry.objects.create(
+            content="sfalekrjo3ijsdfklsj3oi32",
+            creator=self.test_user,
+        )
 
     def tearDown(self):
         self.browser.quit()
         self.test_user.delete()
 
     def logUserIn(self, user=None):
+        """This is a helper function to log a user in with the selenium browser."""
         if not User:
             user = self.test_user
         self.browser.get(self.live_server_url)
@@ -41,18 +54,38 @@ class EntryCreationTest(LiveServerTestCase):
         self.browser.find_element_by_id('submit-login').click()
 
     def test_whether_user_can_create_an_entry_in_microblog(self):
+        """Jerry, a current user, decides he wants to post an entry"""
         test_content = 'SLDFKJEl3kj3ljsdlkfjw'
+        # Jerry logs in
         self.logUserIn()
+        # Jerry navigates to the create entry page
         self.browser.get(self.live_server_url + reverse('microblog:create_entry'))
         self.assertEqual(
             self.browser.current_url,
             self.live_server_url + reverse('microblog:create_entry')
         )
+        # Jerry fills out the form
         self.browser.find_element_by_id('id_content').send_keys(test_content)
         self.browser.find_element_by_id('submit-entry').click()
         # Check that entry was created
+        test_entry = models.Entry.objects.get(content=test_content)
         self.assertEqual(
             test_content,
-            (models.Entry.objects.get(content=test_content)).content,
+            test_entry.content,
+        )
+        # Check that user was redirected to detail page
+        self.assertEqual(
+            self.browser.current_url,
+            self.live_server_url + reverse('microblog:entry_detail', kwargs={'pk': test_entry.pk})
+        )
+        # Jerry looks for his microblog to review his work.
+        self.assertIn(
+            test_entry.content,
+            self.browser.page_source
         )
 
+    def test_whether_user_can_see_other_users_entries(self):
+        """Jenni, a present user, decides to view a friends blog entries."""
+        self.logUserIn(self.test_user2)
+        # TO DO: Jenny should use the search feature to find the user
+        self.fail('Finish Test')
--- twitter_clone/microblog/tests/test_views.py
+++ twitter_clone/microblog/tests/test_views.py
@@ -12,7 +12,6 @@ TEST_USER = {
     'email': 'asdklfjwe@gmail.com',
 }
 
-
 class BlogCreateEntryTest(TransactionTestCase):
 
     def setUp(self):
@@ -48,3 +47,39 @@ class BlogCreateEntryTest(TransactionTestCase):
             test_entry.creator,
             self.test_user
         )
+
+    def test_that_user_is_redirected_to_entry_detail_page(self):
+        test_content = 'SFDKLFJSkwj32qlj'
+        client = Client()
+        client.force_login(self.test_user)
+        data_to_post = {
+            'content': test_content
+        }
+        response = client.post(
+            reverse('microblog:create_entry'),
+            data_to_post
+        )
+        test_entry = models.Entry.objects.get(content=test_content)
+        self.assertRedirects(
+            response,
+            reverse('microblog:entry_detail', kwargs={'pk': test_entry.pk})
+        )
+
+class BlogEntryDetailTest(TransactionTestCase):
+
+    def setUp(self):
+        self.test_user = get_user_model().objects.create_user(
+            TEST_USER['username'], TEST_USER['email'], TEST_USER['password']
+        )
+        self.test_entry = models.Entry.objects.create(
+            content='SDLFJWEwlrwjlwlksfd',
+            creator=self.test_user,
+        )
+
+    def tearDown(self):
+        get_user_model().objects.get(username=TEST_USER['username']).delete()
+
+    def test_whether_tweet_attributes_show_up_on_detail_page(self):
+        client = Client()
+        response = client.get(reverse('microblog:entry_detail', kwargs={'pk': self.test_entry.pk}))
+        self.assertContains(response, self.test_entry.content)
