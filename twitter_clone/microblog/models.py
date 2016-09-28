a498e2420d4a1b65993bc4e920e73d90fa248244
tmcothran <tmcothran@hotmail.com>
2016-09-28 10:33:16 -0400

    Caps micrblog entries at 140 chars in model/datablase.

--- twitter_clone/microblog/models.py
+++ twitter_clone/microblog/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 # Create your models here.
 
 class Entry(models.Model):
-    content = models.CharField(max_length=200)
+    content = models.CharField(max_length=140)
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
