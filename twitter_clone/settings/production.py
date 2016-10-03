5c9e8a823e64589b8cb133ae225af7eaeca6f4a1
tmcothran <tmcothran@hotmail.com>
2016-10-03 11:59:26 -0400

    Fixes databse in production settings.

--- twitter_clone/settings/production.py
+++ twitter_clone/settings/production.py
@@ -2,7 +2,7 @@ import dj_database_url
 
 from .base import *
 
-
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
